50cb74d979ca4baa061a874ef732198155dd8903
Eugene Solomonyk <solosoft2000@mail.ru>
2024-02-28 17:50:36 +0200

    feat: work with findUniqu by id

--- src/routes/graphql/queryType.ts
+++ src/routes/graphql/queryType.ts
@@ -1,5 +1,5 @@
-import { GraphQLList, GraphQLObjectType } from "graphql";
-import { MemberTypesType } from "./types/memberTypes.js";
+import { GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { MemberTypesIdsType, MemberTypesType } from "./types/memberTypes.js";
 import { PostsType } from "./types/posts.js";
 import { ProfilesType } from "./types/profiles.js";
 import { UsersType } from "./types/users.js";
@@ -12,17 +12,53 @@ export const QueryType = new GraphQLObjectType({
       type: new GraphQLList(MemberTypesType),
       resolve: (source, args, context: {prisma: PrismaClient}) => context.prisma.memberType.findMany()
     },
+    memberType: {
+      type: MemberTypesType,
+      args: { id: {type: new GraphQLNonNull(MemberTypesIdsType)} },
+      resolve: async (source, args: { id: string }, context: {prisma: PrismaClient}) => await context.prisma.memberType.findUnique({
+        where: {
+          id: args.id
+        }
+      })
+    },
     posts: {
       type: new GraphQLList(PostsType),
       resolve: (source, args, context: {prisma: PrismaClient}) => context.prisma.post.findMany()
+    },
+    post: {
+      type: PostsType,
+      args: { id: { type: new GraphQLNonNull(GraphQLString) }},
+      resolve: (source, args: { id: string }, context: {prisma: PrismaClient}) => context.prisma.post.findUnique({
+        where: {
+          id: args.id
+        }
+      })
     },
     users: {
       type: new GraphQLList(UsersType),
       resolve: (source, args, context: {prisma: PrismaClient}) => context.prisma.user.findMany()
     },
+    user: {
+      type: UsersType,
+      args: { id: { type: new GraphQLNonNull(GraphQLString) }},
+      resolve: (source, args: { id: string }, context: {prisma: PrismaClient}) => context.prisma.user.findUnique({
+        where: {
+          id: args.id
+        }
+      })
+    },
     profiles: {
       type: new GraphQLList(ProfilesType),
       resolve: (source, args, context: {prisma: PrismaClient}) => context.prisma.profile.findMany()
+    },
+    profile: {
+      type: ProfilesType,
+      args: { id: { type: new GraphQLNonNull(GraphQLString) }},
+      resolve: (source, args: { id: string }, context: {prisma: PrismaClient}) => context.prisma.profile.findUnique({
+        where: {
+          id: args.id
+        }
+      })
     },
   }
 });
--- src/routes/graphql/types/memberTypes.ts
+++ src/routes/graphql/types/memberTypes.ts
@@ -1,10 +1,24 @@
-import { GraphQLFloat, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { GraphQLEnumType, GraphQLFloat, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { MemberTypeId } from "../../member-types/schemas.js";
+
+export const MemberTypesIdsType = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    [MemberTypeId.BASIC]: {
+      value: MemberTypeId.BASIC
+    },
+    [MemberTypeId.BUSINESS]: {
+      value: MemberTypeId.BUSINESS
+    },
+  }
+});
 
 export const MemberTypesType = new GraphQLObjectType({
   name: 'MemberTypes',
   fields: {
-    id: { type: new GraphQLNonNull(GraphQLString) },
+    id: { type: new GraphQLNonNull(MemberTypesIdsType) },
     discount: { type: GraphQLFloat },
     postsLimitPerMonth: { type: GraphQLString }
   }
 });
+

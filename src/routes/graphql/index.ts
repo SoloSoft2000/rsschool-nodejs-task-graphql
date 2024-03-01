9b06dba88903c46a96115ffad964b99d447f3ab9
Eugene Solomonik <solosoft2000@mail.ru>
2024-03-01 08:43:19 +0200

    feat: test rule

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { GraphQLSchema, graphql } from 'graphql';
+import { GraphQLSchema, graphql, validate, parse } from 'graphql';
 import { QueryType } from './queryType.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -20,8 +21,12 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      
       const { query, variables } = req.body;
+      const valid = validate(schema, parse(query), [depthLimit(5)]);
+      
+      if (valid.length) {
+        return { errors: valid };
+      }
       const result = await graphql({
         schema,
         source: query,
